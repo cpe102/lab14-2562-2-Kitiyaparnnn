2ee65a3a1e4c5f4738636e26ce11577749fcfe60
lab314-14@labeng.local <lab314-14@labeng.local>
2020-02-25 15:04:26 +0700

    final

--- lab14_1.cpp
+++ lab14_1.cpp
@@ -1,11 +1,59 @@
-include <iostream>
+#include <iostream>
 using namespace std;
-
 
 int main(){
 	//Write your code here.
-	
-	
-	
+	int a = 5;
+	string b = "A";
+	string &c = b ;
+	int *x = &a, *z=x;
+	string *y = &b;
+
+	cout<<"a = "<<a<<" &a = "<<&a<<"\n";
+	cout<<"b = "<<b<<" &b = "<<&b<<"\n";
+	cout<<"c = "<<c<<" &c = "<<&c<<"\n";
+	cout<<"x = "<<x<<" &x = "<<&x<<"\n";
+	cout<<"y = "<<y<<" &y = "<<&y<<"\n";
+	cout<<"z = "<<z<<" &z = "<<&z<<"\n";
+
+	c="F";
+
+	cout<<"a = "<<a<<"\n";
+	cout<<"b = "<<b<<"\n";
+	cout<<"c = "<<c<<"\n";
+	cout<<"x = "<<x<<"\n";
+	cout<<"y = "<<y<<"\n";
+	cout<<"z = "<<z<<"\n";
+
+	*y="W";
+
+	cout<<"a = "<<a<<"\n";
+	cout<<"b = "<<b<<"\n";
+	cout<<"c = "<<c<<"\n";
+	cout<<"x = "<<x<<"\n";
+	cout<<"y = "<<y<<"\n";
+	cout<<"z = "<<z<<"\n";
+
+
+	*x=6;
+
+	cout<<"a = "<<a<<"\n";
+	cout<<"b = "<<b<<"\n";
+	cout<<"c = "<<c<<"\n";
+	cout<<"x = "<<x<<"\n";
+	cout<<"y = "<<y<<"\n";
+	cout<<"z = "<<z<<"\n";
+
+
+	*z=7;
+
+	cout<<"a = "<<a<<"\n";
+	cout<<"b = "<<b<<"\n";
+	cout<<"c = "<<c<<"\n";
+	cout<<"x = "<<x<<"\n";
+	cout<<"y = "<<y<<"\n";
+	cout<<"z = "<<z<<"\n";
+
+
 	return 0;
 }
